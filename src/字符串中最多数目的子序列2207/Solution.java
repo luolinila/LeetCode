39ab92819530b80c9712260489b2c54dfdad0f39
luolinila <3167296902@qq.com>
2024-09-24 19:14:50 +0800

    测试一下连接两个远端仓库

---
+++ src/字符串中最多数目的子序列2207/Solution.java
@@ -0,0 +1,20 @@
+package 字符串中最多数目的子序列2207;
+
+public class Solution {
+    public long maximumSubsequenceCount(String text, String pattern) {
+        long aCount = 0;
+        long bCount = 0;
+        long count = 0;
+        for(int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == pattern.charAt(1)) {
+                bCount++;
+                count += aCount;
+            }
+            if (text.charAt(i) == pattern.charAt(0)) {
+                aCount++;
+                continue;
+            }
+        }
+        return (long) count + Math.max(aCount, bCount);
+    }
+}

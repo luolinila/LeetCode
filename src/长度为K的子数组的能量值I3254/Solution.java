0458de22728b6e1915e520e5a1aa33e504319bd6
luolinila <3167296902@qq.com>
2024-11-06 17:45:49 +0800

    2024.11.6

--- src/长度为K的子数组的能量值I3254/Solution.java
+++ src/长度为K的子数组的能量值I3254/Solution.java
@@ -30,7 +30,7 @@ public class Solution {
         }
         return res;
     }
-    
+
     public boolean isOK(ArrayList<Integer> nums) {
         for(int i = 0; i  < nums.size() - 1; i++) {
             if(nums.get(i) != nums.get(i + 1) - 1) {

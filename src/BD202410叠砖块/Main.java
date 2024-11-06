aeb356ed9cb2b0533a3d7463056a6278b1b40eb4
luolinila <3167296902@qq.com>
2024-11-06 17:36:08 +0800

    2024.11.6

--- src/BD202410叠砖块/Main.java
+++ src/BD202410叠砖块/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
         // code here
         int n = input.nextInt();
